26b539ee58cc772c1b11ce319abdba6cc2d05f28
mitchashby16 <mitchashby16@gmail.com>
2015-10-18 13:57:48 -0600

    finished blink jquery plugin

--- blink.js
+++ blink.js
@@ -1,1 +1,11 @@
-// YOUR CODE GOES HERE
+$.fn.blink = function(seconds) {
+  var selector = this;
+  blinker(seconds, selector);
+};
+
+
+function blinker(seconds, selector){
+  window.setInterval(function() {
+    selector.toggle();
+  }, seconds);
+}
